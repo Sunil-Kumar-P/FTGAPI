9fa9bfd406b76bc3a8ef94b23513cb2f46c1ba7f
Sunil Kumar P <87166472+Sunil-Kumar-P@users.noreply.github.com>
2024-03-27 13:10:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const { log } = require('console');
-const port = 3000;
+const port = 10000;
 var storedData = [];
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,5 +26,5 @@ app.get('/api/playerposition', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at ${port}`);
 });
