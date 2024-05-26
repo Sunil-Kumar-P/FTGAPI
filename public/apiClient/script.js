976b45cd8dcfc6143993b97f9460b386ed52514a
Sunil Kumar P <sunilkpg23@gmail.com>
2024-05-26 10:02:28 +0530

    updating urls for camera

--- public/apiClient/script.js
+++ public/apiClient/script.js
@@ -183,10 +183,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
         if(rwy < nose){
-          window.location.href = 'http://localhost:3000/FTGv1';
+          window.location.href = 'https://ftgapi.onrender.com:3000/FTGv1';
         }
         if(lwy < nose){
-          window.location.href = 'http://localhost:3000/';
+          window.location.href = 'https://ftgapi.onrender.com:3000/';
         }
 
         // Display position text on the bottom corner
@@ -232,7 +232,7 @@ document.addEventListener("DOMContentLoaded", function () {
   camera.start();
 
   function sendMediaPipePointsToServer(gridPosition) {
-    fetch('http://localhost:3000/api/process', {
+    fetch('https://ftgapi.onrender.com:3000/api/process', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
